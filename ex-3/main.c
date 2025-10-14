d330b2878ccaf0c0fb16c8c653b562a15f3c6e05
Tsuki <alban.elie590@gmail.com>
2025-10-14 10:00:43 +0200

    corection

--- ex-3/main.c
+++ ex-3/main.c
@@ -1,22 +1,19 @@
-
 #include <stdlib.h>
 #include <stdio.h>
-void str_rev(char *str){
+
+void str_rev(char *str) {
     int len = 0;
     while (str[len] != '\0') {
         len++;
     }
-    char* str_re = str;
-    int i = len;
-    while (i != 0){
-        str_re[len - i] = str[i - 1];
-        i = i - 1;
-
+    char str_re[1000000];
+    for (int i = 0; i < len; i++) {
+        str_re[i] = str[len - 1 - i];
     }
- printf("%s\n", str_re);   
+    str_re[len] = '\0';
+    printf("%s\n", str_re);
 }
 
-int main(){
+int main() {
     str_rev("hello");
-
 }
